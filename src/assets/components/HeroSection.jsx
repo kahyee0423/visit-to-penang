cfb3a834c95e929646de001e2f45f73facec6afc
tsehan <tsehantan51@gmail.com>
2024-12-26 00:37:08 +0800

    create about section

--- src/assets/components/HeroSection.jsx
+++ src/assets/components/HeroSection.jsx
@@ -13,13 +13,13 @@ export const HeroSection = () => {
         <p className='text-sm md:text-base'>Your journey begins here</p>
 
         {/* CTA Button */}
-        <button type="button" class="text-white bg-accent-orange hover:bg-accent-orange-hover font-semibold rounded-lg text-md md:text-lg px-3.5 py-2.5 md:px-5 md:py-4 w-fit">
+        <button type="button" className="text-white bg-accent-orange hover:bg-accent-orange-hover font-semibold rounded-lg text-md md:text-lg px-3.5 py-2.5 md:px-5 md:py-4 w-fit">
           Explore Now
         </button>
 
         {/* Image */}
         <div>
-          <img class="h-auto max-w-sm lg:max-w-3xl md:max-w-xl sm:max-w-lg rounded-xl" src="images/mural.jpg" alt="image description"/>
+          <img className="h-auto max-w-sm lg:max-w-3xl md:max-w-xl sm:max-w-lg rounded-xl" src="images/mural.jpg" alt="Penang Mural"/>
         </div>
       </div>
     </div>
