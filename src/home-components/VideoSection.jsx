291ddfb53f52450394ea3c0e68bb48fca7227513
tsehan <tsehantan51@gmail.com>
2025-01-09 22:08:33 +0800

    modify video section's style

--- src/home-components/VideoSection.jsx
+++ src/home-components/VideoSection.jsx
@@ -5,15 +5,19 @@ export const VideoSection = ({
   className = "video",
 }) => {
   return (
-    <div className={`relative ${className} flex justify-center items-center p-12`}> {/* Added padding */}
-      <video 
-        className="w-1/8 h-auto max-w-full sm:max-w-1/3 md:max-w-1/5 lg:max-w-1/6 shadow-lg"  // Even smaller video sizes
-        loop 
-        controls
-      >
-        <source src={videoSrc} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <div className='bg-primary'> {/* Added padding */}
+      <section>
+        <div className="flex text-2xl md:text-3xl font-bold mb-8">A Glimpse of Penang's Wonder</div>
+        <div className={`relative ${className} flex justify-center items-cente`}>
+          <video 
+            className="w-1/8 h-auto max-w-full sm:max-w-1/3 md:max-w-1/5 lg:max-w-1/6 shadow-2xl rounded-xl"  // Even smaller video sizes
+            loop 
+            controls>
+            <source src={videoSrc} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </section>
     </div>
   );
 };
